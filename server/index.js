77cd9f9fe01bab7d7b11e9e623c2b976fff79149
doanngocphuc <doanngocphuc@gmail.com>
2026-03-03 15:07:20 +0700

    force cors header fix

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const http = require("http");
-const cors = require("cors");
 const { Server } = require("socket.io");
 
 const app = express();
 
-/* ===== CORS FIX CHUẨN ===== */
-app.use(cors());
+/* ===== FORCE CORS HEADER ===== */
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 const server = http.createServer(app);
 
